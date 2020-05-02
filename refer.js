d48d9545beb3265bda39964b98162f72e9d493c3
ekaeoq <46292172+ekaeoq@users.noreply.github.com>
2020-05-02 17:02:02 +0200

    Add files via upload

---
+++ refer.js
@@ -0,0 +1,5 @@
+
+
+$(document).ready(function(){
+    $('.logo').delay(200).animate({'marginTop':'+=-40px','opacity':'1'},800);
+});
---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.archlogo').delay(200).animate({'marginTop':'+=-40px','opacity':'1'},800);
+});
+
+function about1() {
+  location.href="indexTheme1.html";
+}
+
+function about2() {
+  location.href="indexTheme2.html";
+}
+
+function about3() {
+  location.href="indexTheme3.html";
+}
